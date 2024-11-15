705d96650618f814a22507d9f9ae9a8e61184eb2
AK Programs <naitik.kashyap5205@gmail.com>
2024-11-15 21:51:33 +0530

    New ✨

--- Advance Codes/fun_code_2.py
+++ Advance Codes/fun_code_2.py
@@ -1,14 +1,12 @@
-# Fun Code-1
+# Fun Code
 print("Can you guess the number of unique alphabets from the given list?")
 values = print("A, A, B, C, D, E, E, F, G, G, G, J, Y, O")
 count = {
     "A", "A", "B", "C", "D", "E", "E", "F", "G", "G", "G", "J", "Y", "O"
 }
 
-# Ask the user to guess the number of unique alphabets
 guess = int(input("Enter how many unique alphabets are written above: "))
 
-# Check if the user's guess is correct or incorrect
 if guess == len(count):
     print("You are correct!")
 else:
