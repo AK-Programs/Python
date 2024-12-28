c03dff264537acebd565abbecfc5bd34a7aaf48f
AK Programs <naitik.kashyap5205@gmail.com>
2024-12-28 11:32:35 +0530

    Updated 🌟

--- Advance Codes/to_do_list.py
+++ Advance Codes/to_do_list.py
@@ -1,36 +1,60 @@
 def to_do_list():
-    to_do_manage = []  
     print("Welcome to the To-Do List Manager!")
 
     while True:
 
         work = input(
             "What do you want to do? "
-            "'Create a To-Do (R)', 'View To-Do List (P)', 'Exit (T)': ")
+            "'Create a To-Do (R)', 'View your To-Do List (P)', 'Clear your To-Do List (C)', 'Exit (T)': ") .strip() .lower()
 
-        if work == "R" or work == "r":
+        if work == "r":
             to_do = input("What do you want to add to your list? ")
-            if to_do:
-                to_do_manage.append(to_do)
+            confirm = input(f"Are you sure you want to add '{to_do}' to your list? Type 'Yes' to confirm: ").strip().lower()
+            if confirm == "yes":
+                f = open("to_do_list_data.txt", "a")
+                f.write(to_do + "\n")
+                f.close()
                 print(f"'{to_do}' has been added to your list.")
             else:
-                print("Error: You cannot add an empty task to the list.")
+                print(f"'{to_do}' has not been added to your list.")
+                
 
-        elif work == "P" or work == "p":
+        elif work == "p":
+            f = open("to_do_list_data.txt", "r")
+            to_do_manage = f.readlines()
             if to_do_manage:
-                print("Your To-Do List:")
                 index = 1  
                 for task in to_do_manage:
-                    print(f"{index}. {task}")
+                    print(f"{index}. {task.strip()}")
                     index += 1  
             else:
                 print("Your To-Do List is empty.")
-
-        elif work == "T" or work == "t":
+            f.close()
+        
+        elif work == "c":
+            f = open("to_do_list_data.txt", "r")
+            to_do_manage = f.readlines()
+            f.close()
+            if to_do_manage:
+                print("Are you sure you want to clear your To-Do List?")
+                confirm = input("Type 'Yes' to confirm: ").strip().lower()
+                if confirm == "yes":
+                    f = open("to_do_list_data.txt", "w")
+                    f.close()
+                    print("Your To-Do List has been cleared.")
+                else:
+                    print("Your To-Do List has not been cleared.")
+                    
+                f = open("to_do_list_data.txt", "w")
+                f.close()
+            else:
+                print("Your To-Do List is already empty.")
+        
+        elif work == "t":
             print("Exiting the To-Do List Manager. Goodbye!")
             break  
 
         else:
-            print("Error: Invalid choice! Please select 'R', 'P', or 'T'.")
+            print("Error: Invalid choice! Please select 'R', 'P', 'C' or 'T'.")
 
 to_do_list()
